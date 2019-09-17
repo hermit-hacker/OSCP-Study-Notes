103c1639bb42cb51a925692bba70c6ccacc97c1c
Hermit <bcmork@gmail.com>
2019-09-17 10:28:07 -0500

    Update smb-rampage.sh

--- smb-rampage.sh
+++ smb-rampage.sh
@@ -1,4 +1,14 @@
 #~/bin/bash
+#
+# Program  : smb-rampage.sh
+# Author   : Brian Mork ("Hermit")
+# Date     : 2019-06-15
+# Contact  : https://blog.stackattack.net
+# Requires : smbclient
+#            smbmap
+# WhatItDo : Simply pulls lists of shares and additional information from
+#            whatever host or IP address is specified as the first and only
+#            argument
 if [ $# -ne 1 ]; then
 	echo "Syntax: SmbRampage.sh [TARGET]"
 	exit 1
